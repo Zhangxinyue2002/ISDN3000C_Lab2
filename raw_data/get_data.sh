2d8681a4eeb8e860341ca388efb91f0cb0185866
Zhangxinyue2002 <xzhangfr@connect.ust.hk>
2025-09-16 18:36:49 +0800

    Add to main

---
+++ raw_data/get_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This is a comment. The line above is a "shebang", telling the OS to use bash.
+
+# This script is using for copy & paste assignment.log in the folder
+
+rm game.log
+curl https://raw.githubusercontent.com/siyanhu/25Fall_ISDN3000C/refs/heads/master/Lab02/assignment.log -o game.log
+mv game.log raw_data/
